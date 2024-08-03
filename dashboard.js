b57ca6fd8a8d8a1d0a0fb795b3187cc7ff52059f
Dulaj Hashmika <dulajhashmika075@gmail.com>
2024-08-04 04:10:36 +0530

    update dashboard page Fixes #11

--- dashboard.js
+++ dashboard.js
@@ -1,15 +1,134 @@
-//! sidebar 
+// SIDEBAR DROPDOWN
+const allDropdown = document.querySelectorAll("#sidebar .side-dropdown");
+const sidebar = document.getElementById("sidebar");
 
-function openNav() {
-  document.getElementById("side-bar").style.width = "300px";
-  document.getElementById("main").style.marginLeft = "300px";
+allDropdown.forEach((item) => {
+  const a = item.parentElement.querySelector("a:first-child");
+  a.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    if (!this.classList.contains("active")) {
+      allDropdown.forEach((i) => {
+        const aLink = i.parentElement.querySelector("a:first-child");
+
+        aLink.classList.remove("active");
+        i.classList.remove("show");
+      });
+    }
+
+    this.classList.toggle("active");
+    item.classList.toggle("show");
+  });
+});
+
+// SIDEBAR COLLAPSE
+const toggleSidebar = document.querySelector("nav .toggle-sidebar");
+const allSideDivider = document.querySelectorAll("#sidebar .divider");
+
+if (sidebar.classList.contains("hide")) {
+  allSideDivider.forEach((item) => {
+    item.textContent = "-";
+  });
+  allDropdown.forEach((item) => {
+    const a = item.parentElement.querySelector("a:first-child");
+    a.classList.remove("active");
+    item.classList.remove("show");
+  });
+} else {
+  allSideDivider.forEach((item) => {
+    item.textContent = item.dataset.text;
+  });
 }
 
-function closeNav() {
-  document.getElementById("side-bar").style.width = "0px";
-  document.getElementById("main").style.marginLeft = "0px";
-}
+toggleSidebar.addEventListener("click", function () {
+  sidebar.classList.toggle("hide");
 
+  if (sidebar.classList.contains("hide")) {
+    allSideDivider.forEach((item) => {
+      item.textContent = "-";
+    });
+
+    allDropdown.forEach((item) => {
+      const a = item.parentElement.querySelector("a:first-child");
+      a.classList.remove("active");
+      item.classList.remove("show");
+    });
+  } else {
+    allSideDivider.forEach((item) => {
+      item.textContent = item.dataset.text;
+    });
+  }
+});
+
+sidebar.addEventListener("mouseleave", function () {
+  if (this.classList.contains("hide")) {
+    allDropdown.forEach((item) => {
+      const a = item.parentElement.querySelector("a:first-child");
+      a.classList.remove("active");
+      item.classList.remove("show");
+    });
+    allSideDivider.forEach((item) => {
+      item.textContent = "-";
+    });
+  }
+});
+
+sidebar.addEventListener("mouseenter", function () {
+  if (this.classList.contains("hide")) {
+    allDropdown.forEach((item) => {
+      const a = item.parentElement.querySelector("a:first-child");
+      a.classList.remove("active");
+      item.classList.remove("show");
+    });
+    allSideDivider.forEach((item) => {
+      item.textContent = item.dataset.text;
+    });
+  }
+});
+
+// PROFILE DROPDOWN
+const profile = document.querySelector("nav .profile");
+const imgProfile = profile.querySelector("img");
+const dropdownProfile = profile.querySelector(".profile-link");
+
+imgProfile.addEventListener("click", function () {
+  dropdownProfile.classList.toggle("show");
+});
+
+// MENU
+const allMenu = document.querySelectorAll("main .content-data .head .menu");
+
+allMenu.forEach((item) => {
+  const icon = item.querySelector(".icon");
+  const menuLink = item.querySelector(".menu-link");
+
+  icon.addEventListener("click", function () {
+    menuLink.classList.toggle("show");
+  });
+});
+
+window.addEventListener("click", function (e) {
+  if (e.target !== imgProfile) {
+    if (e.target !== dropdownProfile) {
+      if (dropdownProfile.classList.contains("show")) {
+        dropdownProfile.classList.remove("show");
+      }
+    }
+  }
+
+  allMenu.forEach((item) => {
+    const icon = item.querySelector(".icon");
+    const menuLink = item.querySelector(".menu-link");
+
+    if (e.target !== icon) {
+      if (e.target !== menuLink) {
+        if (menuLink.classList.contains("show")) {
+          menuLink.classList.remove("show");
+        }
+      }
+    }
+  });
+});
 
 
 
@@ -45,16 +164,18 @@ document.addEventListener("click", (event) => {
 
 //! Add Favorite
 
-const icons = document.querySelectorAll(".like-btn");
+const likeBtns = document.querySelectorAll(".like-btn");
 
-icons.forEach((icon) => {
-  icon.addEventListener("click", function () {
-    if (icon.getAttribute("name") === "bookmark-outline") {
-      icon.setAttribute("name", "bookmark");
-      icon.classList.add("bounce-in");
+likeBtns.forEach((likeBtn) => {
+  likeBtn.addEventListener("click", () => {
+    if (likeBtn.classList.contains("bxs-bookmark")) {
+      likeBtn.classList.remove("bxs-bookmark");
+      likeBtn.classList.add("bx-bookmark");
+      likeBtn.classList.remove("bounce-in");
     } else {
-      icon.setAttribute("name", "bookmark-outline");
-      icon.classList.remove("bounce-in");
+      likeBtn.classList.add("bxs-bookmark");
+      likeBtn.classList.remove("bx-bookmark");
+      likeBtn.classList.add("bounce-in");
     }
   });
 });
--- register_connect.php
+++ register_connect.php
@@ -1,11 +1,11 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"]=="POST") {
-    $first_name = filter_input(INPUT_POST, 'first_name', FILTER_SANITIZE_STRING);
-    $last_name = filter_input(INPUT_POST, 'last_name', FILTER_SANITIZE_STRING);
-    $nic_number= filter_input(INPUT_POST, 'nic_number', FILTER_SANITIZE_STRING);
-    $address = filter_input(INPUT_POST, 'address', FILTER_SANITIZE_STRING);
-    $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
-    $contact_number = filter_input(INPUT_POST, 'contact_number', FILTER_SANITIZE_STRING);
+    $first_name = filter_input(INPUT_POST, 'first_name' );
+    $last_name = filter_input(INPUT_POST, 'last_name' );
+    $nic_number= filter_input(INPUT_POST, 'nic_number');
+    $address = filter_input(INPUT_POST, 'address');
+    $email = filter_input(INPUT_POST, 'email');
+    $contact_number = filter_input(INPUT_POST, 'contact_number');
     $password = isset($_POST['password']) ? $_POST['password'] : null;
     $confirm_password = isset($_POST['confirm_password']) ? $_POST['confirm_password'] : null;
     $birthday = isset($_POST['birthday']) ? $_POST['birthday'] : null;
