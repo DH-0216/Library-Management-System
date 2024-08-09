8776fcf29de3e45c109605d15372fe0d5cc589e2
Dulaj Hashmika <dulajhashmika075@gmail.com>
2024-08-09 20:59:52 +0530

    update

--- JS/togglePassword.js
+++ JS/togglePassword.js
@@ -4,14 +4,14 @@ eyeIcons.forEach((eyeIcon) => {
   eyeIcon.addEventListener("click", () => {
     const passwordInput = eyeIcon
       .closest(".inputbox")
-          .querySelector('input[type = "password"],input[type="text"]');
-      
-      if (passwordInput.type === 'password') {
-          passwordInput.type = 'text';
-          eyeIcon.setAttribute('name', 'eye-outline');
-      } else {
-           passwordInput.type = 'password';
-           eyeIcon.setAttribute('name', 'eye-off-outline');
-      }
+      .querySelector('input[type = "password"],input[type="text"]');
+
+    if (passwordInput.type === "password") {
+      passwordInput.type = "text";
+      eyeIcon.setAttribute("name", "eye-outline");
+    } else {
+      passwordInput.type = "password";
+      eyeIcon.setAttribute("name", "eye-off-outline");
+    }
   });
 });
