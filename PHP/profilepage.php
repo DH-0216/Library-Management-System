ca69a625cfe32ee51733d763f37a0c30e57f1ae0
Dulaj Hashmika <dulajhashmika075@gmail.com>
2024-08-11 18:04:53 +0530

    update

--- PHP/profilepage.php
+++ PHP/profilepage.php
@@ -48,7 +48,7 @@ if (isset($_GET['logout'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
     <link rel="stylesheet" href="../CSS/profilepage.css">
-    <title>Document</title>
+    <title>Profile</title>
 </head>
 
 <body>
